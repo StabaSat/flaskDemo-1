853db1c1b826174fa446f71b37dc6faf52dbfa19
Staba <108194878+StabaSat@users.noreply.github.com>
2022-07-19 22:18:03 +0000

    created the outlook of portfolio

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
-from flask import Flask
+from flask import Flask,render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
-def hello_Staba():
-    return "<p> Hello, Kapardhi! </p>"
+def base_page():
+	return render_template(
+		'home.html',  # Template file path, starting from the templates folder. 
+	)
 
 if __name__=='__main__':
   app.run(host='0.0.0.0', debug=True)
