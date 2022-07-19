abdeabc96df69b29a7c063549c208037b58c5364
Staba <108194878+StabaSat@users.noreply.github.com>
2022-07-19 20:48:53 +0000

    created a website

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
-print('s')
-print("hello staba")
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello_Staba():
+    return "<p> Hello, Kapardhi! </p>"
+
+if __name__=='__main__':
+  app.run(host='0.0.0.0', debug=True)
+
